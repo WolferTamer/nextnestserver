0124ea064b061e7d8d941bc76ce2a7b7d7208d26
Caleb C <wolfertamer@gmail.com>
2025-01-15 13:53:15 -0800

    added port ENV variable

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+let port = process.env.PORT || 3000;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port:3000,
+    port:port,
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:3001',
