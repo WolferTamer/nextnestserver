054a3b120cb3a773f5f6bb3a093ea69344ce3b5f
Caleb C <wolfertamer@gmail.com>
2025-01-15 14:08:08 -0800

    Deployment config react

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,5 @@ let port = process.env.PORT || 3000;
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port:port,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:3001',
-        changeOrigin: true,
-      },
-    },
-  }
+  plugins: [react()]
 })
