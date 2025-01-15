db6adebd0bd4beb4eaa91851415397f8063e6a1b
Caleb C <wolfertamer@gmail.com>
2025-01-15 14:36:18 -0800

    turned proxy server into environment variable.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 let port = process.env.PORT || 3000;
+let APIURL = process.env.API_URL || "http://127.0.0.1:3001"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: APIURL,
+        changeOrigin: true,
+      }
+    }
+  }
 })
